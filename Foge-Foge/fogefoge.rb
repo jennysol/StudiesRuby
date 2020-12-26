bcaf1c7f035082505c2c8e448ce4a8380bd72c3e
jennysol <jennyssoliver@gmail.com>
2020-12-26 11:36:56 -0300

    Lendo Mapa

--- Foge-Foge/fogefoge.rb
+++ Foge-Foge/fogefoge.rb
@@ -1,7 +1,18 @@
 require_relative 'ui'
 
+def le_mapa(numero) 
+    arquivo = "mapa#{numero}.txt"
+    texto = File.read arquivo
+    mapa = texto.split "\n"
+end
+
 def joga(nome)
-    #Nosso jogo aqui
+    mapa = le_mapa 1
+
+    while true
+        desenha mapa
+        direcao = pede_movimento
+    end
 end
 
 def inicia_fogefoge
--- Foge-Foge/ui.rb
+++ Foge-Foge/ui.rb
@@ -6,3 +6,12 @@ def da_boas_vindas
     puts "Começaremos o jogo para você, #{nome}"
     nome
 end
+
+def desenha(mapa) 
+    puts mapa
+end
+
+def pede_movimento
+    puts "Para onde deseja ir ?"
+    movimento = gets.strip
+end
