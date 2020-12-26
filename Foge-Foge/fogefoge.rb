ab03bf5bd27a6659dbb3f2b7933968facf4600f4
jennysol <jennyssoliver@gmail.com>
2020-12-26 11:12:21 -0300

    Iniciando app Fogefoge

---
+++ Foge-Foge/fogefoge.rb
@@ -0,0 +1,10 @@
+require_relative 'ui'
+
+def joga(nome)
+    #Nosso jogo aqui
+end
+
+def inicia_fogefoge
+    nome = da_boas_vindas
+    joga nome
+end
---
+++ Foge-Foge/ui.rb
@@ -0,0 +1,8 @@
+def da_boas_vindas
+    puts "Bem vindo ao Foge-foge"
+    puts "Qual é o seu nome?"
+    nome = gets.strip
+    puts "\n\n\n\n"
+    puts "Começaremos o jogo para você, #{nome}"
+    nome
+end
