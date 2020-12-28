143a0ce7de5041b2a6d0a2795913cb5d2c2d644c
jennysol <jennyssoliver@gmail.com>
2020-12-28 20:59:14 -0300

    Trabalhando movimentos do fantasma

--- Foge-Foge/fogefoge.rb
+++ Foge-Foge/fogefoge.rb
@@ -44,12 +44,31 @@ def posicao_valida?(mapa, posicao)
   if mapa[posicao[0]][posicao[1]] == "X"
       return false
   end
-
   true
 end
 
+def move_fantasma(mapa, linha, coluna)
+  mapa[linha][coluna] = " "
+  linha += 0
+  coluna += 1
+  mapa[linha][coluna] = "F"
+end
+
+def move_fantasmas(mapa)
+  caractere_do_fantasma = "F"
+  mapa.each_with_index do |linha_atual, linha|
+    linha_atual.chars.each_with_index do |caractere_atual, coluna| #chars devolve um array de caracteres
+      eh_fantasma = caractere_atual == caractere_do_fantasma
+      if eh_fantasma
+        move_fantasmas mapa, linha, coluna
+      end
+    end
+  end
+end
+
 def joga(nome)
-  mapa = le_mapa(1)
+  mapa = le_mapa 2
+
   while true
       desenha mapa
       direcao = pede_movimento
@@ -62,6 +81,8 @@ def joga(nome)
 
       mapa[heroi[0]][heroi[1]] = " "
       mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+
+      move_fantasmas mapa
   end
 end
 
