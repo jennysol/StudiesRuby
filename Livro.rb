064baf8a3d3f122658cf23ad404d17e9176dd2f4
jennysol <jennyssoliver@gmail.com>
2021-05-31 11:38:55 -0300

    Update: new business rule added

--- Livro.rb
+++ Livro.rb
@@ -1,20 +1,30 @@
 class Livro
   attr_reader :titulo, :preco, :ano_lancamento
 
-  def initialize(titulo, preco, ano_lancamento)
+  def initialize(titulo, preco, ano_lancamento, possui_reimpressao)
     # @ Variável global main 
     @titulo = titulo
     @preco = preco
     @ano_lancamento = ano_lancamento
-    @preco *= 0.7 if ano_lancamento < 2000
+    @possui_reimpressao = possui_reimpressao
     @preco = calcula_preco(preco)
+  end
+
+  def possui_reimpressao?
+    @possui_reimpressao
   end
 
   private
 
   def calcula_preco(base)
-    if @ano_lancamento < 2000 
-      base * 0.7 
+    if @ano_lancamento < 2006 
+      if @possui_reimpressao 
+        base * 0.9
+      else 
+        base *0.95
+      end
+    elsif @ano_lancamento <= 2010 
+      base * 0.96
     else 
       base
     end
@@ -26,8 +36,9 @@ def livro_para_newsletter(livro)
       puts "Newsletter/Liquidacao"
       puts livro.titulo
       puts livro.preco
+      puts livro.possui_reimpressao?
   end
 end
-algoritmos = Livro.new("Algoritmos", 100, 1998)
+algoritmos = Livro.new("Algoritmos", 100, 1998, true)
 
 livro_para_newsletter(algoritmos)
