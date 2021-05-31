4d26b594cab39cc7590fd689f3d9edf1e80a9961
jennysol <jennyssoliver@gmail.com>
2021-05-31 11:05:42 -0300

    Update: Add behavior control

--- Livro.rb
+++ Livro.rb
@@ -1,19 +1,20 @@
 class Livro
-    attr_accessor :titulo, :preco, :ano_lancamento
+    attr_reader :titulo, :preco, :ano_lancamento
+    # attr_accessor - Leitura e escrita 
+
+    def initialize(titulo, preco, ano_lancamento)
+        @titulo = titulo
+        @preco = preco
+        @ano_lancamento = ano_lancamento
+    end
 end
 
-livro_rails = Livro.new
-livro_rails.preco = 70
-livro_rails.titulo = "Agile Web Development with Rails"
-livro_rails.ano_lancamento = 2011
+livro_rails = Livro.new(70,"Agile Web Development with Rails", 2011 )
 
-
-livro_ruby = Livro.new
-livro_ruby.preco = 60
-livro_ruby.titulo = "Programming Ruby 1.9"
-livro_ruby.ano_lancamento = 2010
+livro_ruby = Livro.new(60, "Programming Ruby 1.9",2010 )
 
 def imprime_nota_fiscal(livros)
+    # each ruby
     livros.each do |livro|
         puts "Titulo: #{livro.titulo} - #{livro.preco}" 
     end
