51a44de235473f8e8407a0768e85ee7c4d65100b
jennysol <jennyssoliver@gmail.com>
2021-05-31 11:22:49 -0300

    Fix: controlling variable body price

--- Livro.rb
+++ Livro.rb
@@ -1,28 +1,33 @@
 class Livro
-    attr_reader :titulo, :preco, :ano_lancamento
-    attr_accessor :preco
+  attr_reader :titulo, :preco, :ano_lancamento
 
-    def initialize(titulo, preco, ano_lancamento)
-        @titulo = titulo
-        @preco = preco
-        @ano_lancamento = ano_lancamento
+  def initialize(titulo, preco, ano_lancamento)
+    # @ Variável global main 
+    @titulo = titulo
+    @preco = preco
+    @ano_lancamento = ano_lancamento
+    @preco *= 0.7 if ano_lancamento < 2000
+    @preco = calcula_preco(preco)
+  end
+
+  private
+
+  def calcula_preco(base)
+    if @ano_lancamento < 2000 
+      base * 0.7 
+    else 
+      base
     end
+  end
 end
 
 def livro_para_newsletter(livro)
-    if livro.ano_lancamento < 1999
-        puts "Newsletter/Liquidacao"
-        puts livro.titulo
-        puts livro.preco
-    end
+  if livro.ano_lancamento < 1999
+      puts "Newsletter/Liquidacao"
+      puts livro.titulo
+      puts livro.preco
+  end
 end
 algoritmos = Livro.new("Algoritmos", 100, 1998)
 
-
-def aplica_promocoes(livro)
-    if livro.ano_lancamento < 2000
-        livro.preco *= 0.7
-    end
-end
-aplica_promocoes(algoritmos)
 livro_para_newsletter(algoritmos)
