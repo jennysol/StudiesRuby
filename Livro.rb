975bc226cb1bc1eee9ea161ad31bd1d3ad857001
jennysol <jennyssoliver@gmail.com>
2021-06-02 20:32:16 -0300

    Feature: Add class Float

--- Livro.rb
+++ Livro.rb
@@ -61,6 +61,14 @@ class Conversor
   end
 end
 
+class Float
+  def para_dinheiro
+    valor = "R$" << self.to_s.tr('.', ',')
+    valor << "0" unless valor.match /(.*)(\d{2})$/
+    valor
+  end
+end
+
 class Estoque 
   attr_reader :livros
 
@@ -111,3 +119,11 @@ estoque <<  ruby
 puts estoque.maximo_necessario
 estoque.remove algoritmos
 puts estoque.maximo_necessario
+
+puts precos = [30.00, 40.00, 70.00, 59.00]
+precos_string = []
+precos.each do |preco|
+  puts preco.class
+  precos_string << preco.para_dinheiro
+end
+puts precos_string.to_s
