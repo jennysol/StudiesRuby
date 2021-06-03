7594e59d6328c64977be2f3ac7f5fd3573b7b17f
jennysol <jennyssoliver@gmail.com>
2021-06-03 16:26:08 -0300

    Fix: refactoring through function extraction

--- OrientaçaoObjetos/Estoque.rb
+++ OrientaçaoObjetos/Estoque.rb
@@ -9,23 +9,15 @@ class Estoque
     @livros.extend Contador
   end
 
-  def quantidade_de_vendas_por(produto, &campo)
-      @vendas.count { |venda| campo.call(venda) == campo.call(produto) }
-  end
-
-  def livro_que_mais_vendeu_por(&campo)
-    que_mais_vendeu_por("livro",&campo)
-  end
-
-  def revista_que_mais_vendeu_por(&campo)
-    que_mais_vendeu_por("revista", &campo)
-  end
-
-  def que_mais_vendeu_por(tipo, &campo)
-    @vendas.select {|l| l.tipo == tipo }.sort {|v1, v2| 
-      quantidade_de_vendas_por(v1, &campo) <=> 
-      quantidade_de_vendas_por(v2, &campo)
-    }.last
+  def method_missing(name)
+    mather = name.to_s.match "(.+)_que_mais_vendeu_por_(.+)"
+    if mather
+      tipo = mather[1]
+      campo = mather[2].to_sym
+      que_mais_vendeu_por(tipo, &campo)
+    else
+      super
+    end
   end
 
   def exporta_csv
@@ -49,7 +41,21 @@ class Estoque
     @livros.delete livro
     @vendas << livro
   end
+
   def maximo_necessario
     @livros.maximo_necessario
+  end
+
+  private
+
+  def quantidade_de_vendas_por(produto, &campo)
+    @vendas.count { |venda| campo.call(venda) == campo.call(produto) }
+  end
+
+  def que_mais_vendeu_por(tipo, &campo)
+    @vendas.select {|l| l.tipo == tipo }.sort {|v1, v2| 
+      quantidade_de_vendas_por(v1, &campo) <=> 
+      quantidade_de_vendas_por(v2, &campo)
+    }.last
   end
 end
--- OrientaçaoObjetos/sistema.rb
+++ OrientaçaoObjetos/sistema.rb
@@ -21,16 +21,21 @@ programmer = Livro.new("The Progmatic Programmer", 100, 1999, true, "", "livro")
 ruby = Livro.new("Programming Ruby", 100, 2004, true, "", "livro")
 
 revistona = Livro.new("Revista de Ruby", 10, 2012, true, "Revistas", "revista")
+online_arquitetura = Livro.new("Introdução a Arquitetura e Design de Software", 50, 2012, true, "", "ebook")
+
 estoque = Estoque.new
-estoque << algoritmos << algoritmos << ruby << programmer << arquitetura << ruby << ruby << revistona << revistona
+estoque << algoritmos << algoritmos << ruby << programmer << arquitetura << ruby << ruby << revistona << online_arquitetura
 
 estoque.vende ruby
 estoque.vende algoritmos
 estoque.vende algoritmos
 estoque.vende revistona
+estoque.vende online_arquitetura
 
-puts estoque.livro_que_mais_vendeu_por(&:titulo).titulo
-puts estoque.revista_que_mais_vendeu_por(&:titulo).titulo
+puts estoque.livro_que_mais_vendeu_por_titulo.titulo
+puts estoque.revista_que_mais_vendeu_por_titulo.titulo
+puts estoque.ebook_que_mais_vendeu_por_titulo.titulo
+
 
 
 
