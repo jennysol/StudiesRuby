6b9243028c9a2fad7cf430280f7568e194b81c6b
jennysol <jennyssoliver@gmail.com>
2021-06-07 19:53:06 -0300

    Feature: add module produto to revista and ebook

--- OrientaçaoObjetos/Ebook.rb
+++ OrientaçaoObjetos/Ebook.rb
@@ -1,5 +1,8 @@
+require_relative "Produto"
 class Ebook
     attr_reader :titulo, :preco, :ano_lancamento, :editora
+
+    include Produto
   
     def initialize(titulo, preco, ano_lancamento, editora)
       # @ Variável global main 
@@ -13,26 +16,6 @@ class Ebook
 
     def matches?(query)
       ["ebook", "digital"].include?(query)
-    end
-  
-    def to_csv
-      "#{@titulo}, #{@ano_lancamento}, #{@preco}"
-    end
-  
-    private
-  
-    def calcula_preco(base)
-      if @ano_lancamento < 2006 
-        if @possui_reimpressao 
-          base * 0.9
-        else 
-          base *0.95
-        end
-      elsif @ano_lancamento <= 2010 
-        base * 0.96
-      else 
-        base
-      end
     end
   end
   
